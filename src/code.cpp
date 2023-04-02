1f8c24eafc4fc7e89b98c645ca0ba0c1bcb0157a
am <am2022a2@gmail.com>
2023-04-02 11:57:53 +0430

    edit code_string function

--- src/code.cpp
+++ src/code.cpp
@@ -1,5 +1,22 @@
 #include "code.h"
 
 string code::code_string(string s){
-    
+    char buff;
+    string out;
+
+    for(int i = 0;i < s.length();i++){
+        buff = s[i];
+        
+        if(isalpha(buff)){
+            if(tolower(buff) - 'a' < 14){
+                out.append(1, buff + 13);
+            }
+            else{
+                out.append(1, buff - 13);
+            }
+        }
+        
+    }
+
+    return out;
 }
