cbec61dceafea16a8ce5a902ea778ece20b8ca35
am <am2022a2@gmail.com>
2023-04-02 12:32:59 +0430

    ask user for text and show the rot13 of that

--- src/rot13.cpp
+++ src/rot13.cpp
@@ -7,6 +7,12 @@ using namespace std;
 int main(){
     code c;
     decode dc;
-    
+
+    string code;
+
+    cout<<"enter the text that you want:";
+    cin>>code;
+    cout<<"result:"<<c.code_string(code);
+
     return 0;
 }
