529572872f7479b3b3722d1616b0aeb7d48fb6f7
am <am2022a2@gmail.com>
2023-07-04 09:12:02 +0330

    ask user for input file in rot13.cpp

--- src/rot13.cpp
+++ src/rot13.cpp
@@ -8,7 +8,11 @@ int main(){
     code c;
 
     string code;
+    string a_file_in;
 
+    cout<<"do you want to import file for input(y/n):";
+    cin>>a_file_in;
+    
     cout<<"enter the text that you want:";
     getline(cin, code);
     cout<<"result:"<<c.code_string(code);
