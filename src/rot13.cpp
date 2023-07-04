c6d2babfe70e19b49ac5887d4e50ffb7326ffc3f
am <am2022a2@gmail.com>
2023-07-04 09:15:26 +0330

    gets file name from user (in rot13.cpp)

--- src/rot13.cpp
+++ src/rot13.cpp
@@ -9,10 +9,15 @@ int main(){
 
     string code;
     string a_file_in;
+    string file_in;
 
     cout<<"do you want to import file for input(y/n):";
     cin>>a_file_in;
-    
+    if(a_file_in[0] == 'Y' || a_file_in[0] == 'y'){
+        cout<<"enter file name:";
+        cin>>file_in;
+    }
+
     cout<<"enter the text that you want:";
     getline(cin, code);
     cout<<"result:"<<c.code_string(code);
