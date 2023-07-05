d6549b3f2bf5356dc83f0b46fb62faffa759a92a
am <am2022a2@gmail.com>
2023-07-05 08:06:15 +0330

    gets name of the output file

--- src/rot13.cpp
+++ src/rot13.cpp
@@ -11,6 +11,7 @@ int main(){
     string a_file_in;
     string file_in;
     string a_file_out;
+    string file_out;
 
     cout<<"do you want to import file for input(y/n):";
     cin>>a_file_in;
@@ -19,6 +20,10 @@ int main(){
         cin>>file_in;
         cout<<"do you want to export output(y/n):";
         cin>>a_file_out;
+        if(a_file_out[0] == 'Y' || a_file_out[0] == 'y'){
+            cout<<"enter file name:";
+            cin>>file_out;
+        }
     }
 
     cout<<"enter the text that you want:";
