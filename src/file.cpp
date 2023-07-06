ff063ac2344f406430a4d3aad70f0444fdb8db88
am <am2022a2@gmail.com>
2023-07-06 10:03:24 +0330

    new var value set (in file.cpp)

--- src/file.cpp
+++ src/file.cpp
@@ -3,8 +3,10 @@
 file::file(string fname_in, string fname_out){
     this->file_name_in.open(fname_in);
     this->file_name_out.open(fname_out);
+    this->fnameout_entered = 1;
 }
 
 file::file(string fname_in){
     this->file_name_in.open(fname_in);
+    this->fnameout_entered = 0;
 }
