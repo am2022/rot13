387f214798c55879dd27cc141a7040ea11dddbce
am <am2022a2@gmail.com>
2023-07-06 10:20:41 +0330

    is_fname_in_valid() created (in file.cpp)

--- src/file.cpp
+++ src/file.cpp
@@ -10,3 +10,14 @@ file::file(string fname_in){
     this->file_name_in.open(fname_in);
     this->fnameout_entered = 0;
 }
+
+int file::is_fname_in_valid(string fname_in){
+    ifstream f_test(fname_in);
+
+    if(! f_test){
+        return 0;
+    }
+    else{
+        return 1;
+    }
+}
