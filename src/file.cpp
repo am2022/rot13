9e83f2fd5153a477662dd62cb6e8451c8e394b7f
am <am2022a2@gmail.com>
2023-07-06 10:44:54 +0330

    is_fname_in_valid() used in file(string, string)

--- src/file.cpp
+++ src/file.cpp
@@ -1,9 +1,15 @@
 #include "file.h"
 
 file::file(string fname_in, string fname_out){
-    this->file_name_in.open(fname_in);
     this->file_name_out.open(fname_out);
     this->fnameout_entered = 1;
+    if(! this->is_fname_in_valid(fname_in)){
+        cout<<"file name is invalid!\n";
+        return;
+    }
+    else{
+        this->file_name_in.open(fname_in);
+    }
 }
 
 file::file(string fname_in){
