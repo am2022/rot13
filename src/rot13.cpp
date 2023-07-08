bc5d33b4bc5ff2eac3ab09ac44ece229244c46e5
am <am2022a2@gmail.com>
2023-07-08 22:07:09 +0330

    parts for working with files deleted (in rot13.cpp)

--- src/rot13.cpp
+++ src/rot13.cpp
@@ -1,34 +1,12 @@
 #include <iostream>
 #include <string>
 #include "code.cpp"
-#include "file.cpp"
 using namespace std;
 
 int main(){
     code c;
 
     string code;
-    string a_file_in;
-    string file_in;
-    string a_file_out;
-    string file_out;
-
-    cout<<"do you want to import file for input(y/n):";
-    cin>>a_file_in;
-    if(a_file_in[0] == 'Y' || a_file_in[0] == 'y'){
-        cout<<"enter file name:";
-        cin>>file_in;
-        cout<<"do you want to export output(y/n):";
-        cin>>a_file_out;
-        if(a_file_out[0] == 'Y' || a_file_out[0] == 'y'){
-            cout<<"enter file name:";
-            cin>>file_out;
-            file fout(file_in, file_out);
-        }
-        else{
-            file fout(file_in);
-        }
-    }
 
     cout<<"enter the text that you want:";
     getline(cin, code);
